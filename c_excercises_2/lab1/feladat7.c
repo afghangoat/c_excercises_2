59429a4ab09d385bdb92a635560930248a9b6797
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-06-23 19:35:58 +0200

    Initial commit - Mirrored from my site, file: c_excercises_2/lab1/feladat7.c

---
+++ c_excercises_2/lab1/feladat7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+double PI=3.1415926;
+
+
+
+//FELADAT 7
+int main(){
+    int i=0;
+    while(i<=20){
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
