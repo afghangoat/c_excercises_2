ea46c95a2c83d62504a91fc8872598fa0ee47236
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-06-23 19:39:36 +0200

    Initial commit - Mirrored from my site, file: c_excercises_2/lab8/main3.c

---
+++ c_excercises_2/lab8/main3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "debugmalloc.h"
+
+int main(void) {
+    int n=0;
+    printf("Hány szmáot szeretnél beolvasni?:\n");
+    scanf("%d",&n);
+    double *szamok= (double*) malloc(sizeof(double)*n);
+    for(int i=0;i<n;i++){
+        double v=0.0;
+        scanf("%lf",&v);
+        szamok[i]=v;
+    }
+    for(int i=0;i<n;i++){
+        printf("%lf",szamok[n-i-1]);
+    }
+    
+    free(szamok);
+    return 0;
+}
